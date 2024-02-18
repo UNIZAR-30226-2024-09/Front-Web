b9013c0cb750ba218fec7b6e35efa885247d0892
sinnuvva <819684@unizar.es>
2024-02-18 14:57:46 +0100

    proyecto_montado_correcto

--- musify/src/App.js
+++ musify/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import inicio_sesion from "./componentes/inicio_sesion/inicio_sesion";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/iniciarSesion" element={<inicio_sesion />} />
+        </Routes>
+      </BrowserRouter>
+    );  
+  }
 }
-
-export default App;
